d1f8682b55bd1456e02fda338c711ac5f5258736
kenni kollemorten <kenn7575@gmail.com>
2024-01-31 16:10:42 +0100

    added privacy policy

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<main>
+	<iframe
+		type="application/pdf"
+		class="w-full min-h-view"
+		src="/InstaMatch privacy.pdf"
+		title="privacy policy"
+		frameborder="0"
+	></iframe>
+</main>
