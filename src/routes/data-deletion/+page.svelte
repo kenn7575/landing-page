ea3cc5aa5ce2a5e7b5c978e79433cf23baf139c0
kenni kollemorten <kenn7575@gmail.com>
2024-02-10 21:50:28 +0100

    Add data deletion page and PDF file

---
+++ src/routes/data-deletion/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<iframe
+	title="data deletion"
+	class="w-full h-view"
+	src="https://docs.google.com/gview?url=http://instamatch.dk/data-deletion.pdf&embedded=true"
+	style="width:718px; height:700px;"
+	frameborder="0"
+></iframe>
